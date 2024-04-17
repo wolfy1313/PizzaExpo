906b55f2d3240ae372d6011bf67aece69f311d89
jeremyHarrell <hillvulf@gmail.com>
2024-04-17 15:27:45 -0500

    pull in products array from assets, use .find method to use id of product relative to the id in params, display product name as title

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,13 +1,15 @@
 import { useLocalSearchParams, Stack } from 'expo-router'
 import React from 'react'
 import { View, Text } from 'react-native'
+import products from '@/assets/data/products'
 
 const ProductDetailsScreen = () => {
   const { id } = useLocalSearchParams()
+  const product = products.find((p) => p.id.toString() === id)
 
   return (
     <View>
-      <Stack.Screen options={{ title: 'Details' }} />
+      <Stack.Screen options={{ title: product?.name }} />
       <Text style={{ fontSize: 20 }}>ProductDetailsScreen for id: {id}</Text>
     </View>
   )
