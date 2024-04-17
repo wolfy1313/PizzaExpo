b283bbabf0c5aa1de91a1294ee4530cfe05990e5
jeremyHarrell <hillvulf@gmail.com>
2024-04-17 16:18:53 -0500

    add StyleSheet and scaffold styles

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,18 +1,30 @@
 import { useLocalSearchParams, Stack } from 'expo-router'
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Image, StyleSheet } from 'react-native'
 import products from '@/assets/data/products'
+import { defaultPizzaImage } from '@/src/components/ProductListItem'
 
 const ProductDetailsScreen = () => {
   const { id } = useLocalSearchParams()
   const product = products.find((p) => p.id.toString() === id)
 
+  if (!product) {
+    return <Text>Product not found</Text>
+  }
+
   return (
-    <View>
-      <Stack.Screen options={{ title: product?.name }} />
+    <View style={styles.container}>
+      <Stack.Screen options={{ title: product.name }} />
+      <Image source={{ uri: product.image || defaultPizzaImage }} style={ } />
       <Text style={{ fontSize: 20 }}>ProductDetailsScreen for id: {id}</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {},
+  image: {},
+  price: {}
+})
 
 export default ProductDetailsScreen;
