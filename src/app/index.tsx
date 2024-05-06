d742906462a486645187e50687c5b73f88b70f13
jeremyHarrell <hillvulf@gmail.com>
2024-05-06 16:58:31 -0500

    create auth folder and sign-in.tsx, sign-up.tsx and layout.tsx. build out layout.tsx to render Stack

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -12,6 +12,9 @@ const index = () => {
       <Link href={'/(admin)'} asChild>
         <Button text="Admin" />
       </Link>
+      <Link href={'/sign-in'} asChild>
+        <Button text="Sign In" />
+      </Link>
     </View>
   );
 };
