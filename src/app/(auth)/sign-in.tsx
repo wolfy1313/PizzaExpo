3a47a0be530b0106ab278072e6b6d845ca61be98
jeremyHarrell <hillvulf@gmail.com>
2024-05-07 15:36:14 -0500

    add stylesheet from create.tsx to sign-in.tsx

--- src/app/(auth)/sign-in.tsx
+++ src/app/(auth)/sign-in.tsx
@@ -1,7 +1,10 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import Colors from '@/src/constants/Colors'
 
 const SignInScreen = () => {
+
+
   return (
     <View>
       <Text>sign-in</Text>
@@ -11,4 +14,32 @@ const SignInScreen = () => {
 
 export default SignInScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 10,
+  },
+  image: {
+    width: '50%',
+    aspectRatio: 1,
+    alignSelf: 'center'
+  },
+  textButton: {
+    alignSelf: 'center',
+    fontWeight: 'bold',
+    color: Colors.light.tint,
+    marginVertical: 10
+  },
+  input: {
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 5,
+    marginBottom: 20,
+    marginTop: 5,
+  },
+  label: {
+    color: 'gray',
+    fontSize: 16,
+  },
+})
