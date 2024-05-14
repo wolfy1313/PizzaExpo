a4ba641eb85811232833e842489db9fe3c36f227
jeremyHarrell <hillvulf@gmail.com>
2024-05-14 15:07:25 -0500

    add OrderListItemProps to OrderListItem.tsx / install dayjs to display human readable times on orders

--- src/components/OrderListItem.tsx
+++ src/components/OrderListItem.tsx
@@ -1,7 +1,15 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { Order } from '../types'
+import { useSegments } from 'expo-router'
 
-const OrderListItem = () => {
+type OrderListItemProps = {
+  order: Order
+}
+
+const OrderListItem = ({ order }: OrderListItemProps) => {
+  const segments = useSegments()
+  const dayjs = require('dayjs')
   return (
     <View>
       <Text>OrderListItem</Text>
