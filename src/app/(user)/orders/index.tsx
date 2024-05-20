f03ed0d9031db6010dff0eb5871d90c64ab421a0
jeremyHarrell <hillvulf@gmail.com>
2024-05-20 11:13:06 -0500

    update imports in user orders index file

--- src/app/(user)/orders/index.tsx
+++ src/app/(user)/orders/index.tsx
@@ -1,4 +1,4 @@
-import { FlatList, StyleSheet, Text, View } from 'react-native'
+import { FlatList, StyleSheet } from 'react-native'
 import React from 'react'
 import orders from '@/assets/data/orders'
 import OrderListItem from '@/src/components/OrderListItem'
