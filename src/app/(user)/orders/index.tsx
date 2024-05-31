ec312d1b5a2228249ea63a05b14b5b964f94389a
jeremyHarrell <hillvulf@gmail.com>
2024-05-31 15:06:25 -0500

    update (user)/orders/index.tsx to render OrderListItems correctly

--- src/app/(user)/orders/index.tsx
+++ src/app/(user)/orders/index.tsx
@@ -11,9 +11,7 @@ export default function OrdersScreen() {
       <FlatList
         data={orders}
         renderItem={({ item }) => <OrderListItem order={item} />}
-        numColumns={2}
         contentContainerStyle={{ gap: 10, padding: 10 }}
-        columnWrapperStyle={{ gap: 10 }}
       />
     </>
   )
