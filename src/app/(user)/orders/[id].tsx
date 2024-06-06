cfbaa43a993df1d4aece11e5bdeeb1139881f636
jeremyHarrell <hillvulf@gmail.com>
2024-06-06 12:15:37 -0500

    add image to orderItemListItem on details page

--- src/app/(user)/orders/[id].tsx
+++ src/app/(user)/orders/[id].tsx
@@ -1,8 +1,9 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { FlatList, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { Stack, useLocalSearchParams } from 'expo-router'
 import orders from '@/assets/data/orders'
 import OrderListItem from '@/src/components/OrderListItem'
+import OrderItemListItem from '@/src/components/OrderItemListItem'
 
 const OrderDetailsScreen = () => {
   const { id } = useLocalSearchParams()
@@ -19,6 +20,10 @@ const OrderDetailsScreen = () => {
     <View style={styles.container}>
       <Stack.Screen options={{ title: `Order #${order.id}` }} />
       <OrderListItem order={order} />
+      <FlatList
+        data={order.order_items}
+        renderItem={({ item }) => <OrderItemListItem item={item} />}
+      />
     </View>
   )
 }
--- src/components/OrderItemListItem.tsx
+++ src/components/OrderItemListItem.tsx
@@ -1,19 +1,50 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { OrderItem } from '../types';
+import Colors from '../constants/Colors';
+import { defaultPizzaImage } from './ProductListItem';
+import { Image } from 'react-native';
 
 type OrderItemListItemProps = {
-  orderItem: OrderItem;
+  item: OrderItem;
 }
 
-const OrderItemListItem = ({ orderItem }: OrderItemListItemProps) => {
+const OrderItemListItem = ({ item }: OrderItemListItemProps) => {
   return (
-    <View>
-      <Text>OrderItemListItem</Text>
+    <View style={styles.container}>
+      <Image
+        source={{ uri: item.products.image || defaultPizzaImage }}
+        style={styles.image}
+        resizeMode="contain"
+      />
     </View>
   )
 }
 
 export default OrderItemListItem
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    borderRadius: 10,
+    padding: 5,
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  image: {
+    width: 75,
+    aspectRatio: 1,
+    alignSelf: 'center',
+    marginRight: 10,
+  },
+  title: {
+    fontWeight: '500',
+    fontSize: 16,
+    marginBottom: 5,
+  },
+  price: {
+    color: Colors.light.tint,
+    fontWeight: 'bold',
+  },
+})
