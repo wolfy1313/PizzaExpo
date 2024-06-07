340f0119633e7c77dc73a764c2a3b93b4e95a917
jeremyHarrell <hillvulf@gmail.com>
2024-06-07 16:08:29 -0500

    install expo brightness

---
+++ src/components/UseMaxBrightness.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export function useMaxBrightness(disabled?: boolean = false) {
+  return (
+    <View>
+      <Text>UseMaxBrightness</Text>
+    </View>
+  )
+}
+
+export default useMaxBrightness
