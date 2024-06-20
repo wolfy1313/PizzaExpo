8f1d24908c083db5b4e041617c2424099242ee66
jeremyHarrell <hillvulf@gmail.com>
2024-06-20 13:28:39 -0500

    create new account on sign up screen

--- src/app/(auth)/sign-up.tsx
+++ src/app/(auth)/sign-up.tsx
@@ -1,14 +1,27 @@
-import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import { Alert, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import React, { useState } from 'react'
 import Colors from '@/src/constants/Colors'
 import Button from '@/src/components/Button'
 import { Stack, Link } from 'expo-router'
+import { supabase } from '@/src/lib/supabase'
 
 
 const SignUpScreen = () => {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
   const [hidePass, setHidePass] = useState(true)
+  const [loading, setLoading] = useState(false)
+
+  async function signUpWithEmail() {
+    setLoading(true)
+    const { error } = await supabase.auth.signUp({
+      email: email,
+      password: password
+    })
+
+    if (error) Alert.alert(error.message)
+    setLoading(false)
+  }
 
   const resetFields = () => {
     setEmail("");
