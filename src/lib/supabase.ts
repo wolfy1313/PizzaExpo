d3f6af2d86cceedfd4e74ffc29fed4f518695895
jeremyHarrell <hillvulf@gmail.com>
2024-06-21 13:16:57 -0500

    add supabase url and supabase anon key

---
+++ src/lib/supabase.ts
@@ -0,0 +1,27 @@
+import 'react-native-url-polyfill/auto';
+import * as SecureStore from 'expo-secure-store';
+import { createClient } from '@supabase/supabase-js';
+
+const ExpoSecureStoreAdapter = {
+  getItem: (key: string) => {
+    return SecureStore.getItemAsync(key);
+  },
+  setItem: (key: string, value: string) => {
+    SecureStore.setItemAsync(key, value);
+  },
+  removeItem: (key: string) => {
+    SecureStore.deleteItemAsync(key);
+  },
+};
+
+const supabaseUrl = 'https://iahptkxlibecxotqiifo.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlhaHB0a3hsaWJlY3hvdHFpaWZvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjEyMzcyODIsImV4cCI6MjAzNjgxMzI4Mn0.DeEhVjS4B1ENLI3C97v8xUF7qkikMs-_orB4H85Usqg';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: ExpoSecureStoreAdapter as any,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
