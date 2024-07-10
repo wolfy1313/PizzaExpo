025492a54289cc22983cdee7c5b5a68dffafa080
jeremyHarrell <hillvulf@gmail.com>
2024-07-10 18:08:09 -0500

    update name of archive tab

--- src/app/(admin)/orders/list/archive.tsx
+++ src/app/(admin)/orders/list/archive.tsx
@@ -7,7 +7,7 @@ import { Stack } from 'expo-router'
 export default function OrdersScreen() {
   return (
     <>
-      <Stack.Screen options={{ title: "Orders" }} />
+      <Stack.Screen options={{ title: "Archive" }} />
       <FlatList
         data={orders}
         renderItem={({ item }) => <OrderListItem order={item} />}
